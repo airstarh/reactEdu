21d5a32f37e18582dcf739d8d1c129d5bb1e9e8a
V at gmail <vsevolod.azovsky@gmail.com>
2018-07-26 23:22:13 +0300

    Dev. After state.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -26,8 +26,11 @@ class Square extends React.Component {
 
 	render() {
 		return (
-			<button className="square" onClick={()=>{ console.log(this.props);}}>
-				{this.props.value}
+			<button
+				className="square"
+				onClick={() => this.setState({value: 'X'})}
+			>
+				{this.state.value}
 			</button>
 		);
 	}
